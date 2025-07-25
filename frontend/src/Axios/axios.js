a77c92ea26e5b9bfce10c6752c68b176faeb49da
ericjazmawira <ericjazmawira@gmail.com>
2025-07-25 16:41:24 +0300

    Fixed an exposee in Axios

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,7 @@
-import axios from "axios"
+import axios from "axios";
+
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
-})
-export default instance
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+});
+
+export default instance;
